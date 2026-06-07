79bae0a380e0cb2daf6cf230651d0f8bedfe6e31
Sanjib Behera <178703817+sanjib2006@users.noreply.github.com>
2026-06-07 11:19:52 +0530

    [Connectors] Refactor YETI to API v2 payload and auth schemas (#3736)
    * refactor(connectors): update YETI to API v2 payload and auth schemas
    
    * refactor(connectors): YETI observable type handling
    
    * fix(yeti): update context date to received_request_time to avoid race condition
    
    * test(yeti): add unit tests for YETI connector auth and response handling
    
    * YETI: Add IntelOwl as User-Agent to request headers

--- api_app/connectors_manager/connectors/yeti.py
+++ api_app/connectors_manager/connectors/yeti.py
@@ -1,6 +1,8 @@
 # This file is a part of IntelOwl https://github.com/intelowlproject/IntelOwl
 # See the file 'LICENSE' for copying permission.
 
+import ipaddress
+
 import requests
 from django.conf import settings
 
@@ -23,12 +25,30 @@ class YETI(classes.Connector):
             obs_value = self._job.analyzable.name
             obs_type = self._job.analyzable.classification
 
+        # convert obs_type to YETI's expected types if possible
+        if obs_type == "ip":
+            # mark whether the IP is ipv4 or ipv6, fallback to generic on error
+            try:
+                ip_obj = ipaddress.ip_address(obs_value)
+                if ip_obj.version == 4:
+                    obs_type = "ipv4"
+                elif ip_obj.version == 6:
+                    obs_type = "ipv6"
+                else:
+                    obs_type = "generic"
+            except Exception:
+                obs_type = "generic"
+        elif obs_type == "domain":
+            obs_type = "hostname"
+        elif obs_type == "hash":
+            obs_type = "generic"
+
         # create context
         context = {
             "source": "IntelOwl",
             "report": f"{settings.WEB_CLIENT_URL}/jobs/{self.job_id}",
             "status": "analyzed",
-            "date": str(self._job.finished_analysis_time),
+            "date": str(self._job.received_request_time),
             "description": f"IntelOwl's analysis report for Job: {self.job_id} | {obs_value} | {obs_type}",
             "analyzers executed": ", ".join(
                 list(self._job.analyzers_to_execute.all().values_list("name", flat=True))
@@ -40,18 +60,46 @@ class YETI(classes.Connector):
 
         # request payload
         payload = {
-            "value": obs_value,
-            "source": "IntelOwl",
             "tags": tags,
-            "context": context,
+            "observable": {
+                "type": obs_type,
+                "value": obs_value,
+                "context": [context],
+            },
         }
-        headers = {"Accept": "application/json", "X-Api-Key": self._api_key_name}
+
         if self._url_key_name and self._url_key_name.endswith("/"):
             self._url_key_name = self._url_key_name[:-1]
-        url = f"{self._url_key_name}/api/v2/observables/"
+
+        # auth
+        auth_url = f"{self._url_key_name}/api/v2/auth/api-token"
+        auth_headers = {"x-yeti-apikey": self._api_key_name, "User-Agent": "IntelOwl"}
+
+        try:
+            auth_resp = requests.post(
+                url=auth_url,
+                headers=auth_headers,
+                verify=self.verify_ssl,
+                timeout=60,
+            )
+            auth_resp.raise_for_status()
+            access_token = auth_resp.json().get("access_token")
+
+            if not access_token:
+                raise ConnectorRunException("Failed to obtain access token from YETI.")
+        except requests.RequestException as e:
+            raise ConnectorRunException(f"YETI Auth Request failed: {e}")
 
         # create observable with `obs_value` if it doesn't exists
         # new context, tags, source are appended with existing ones
+
+        url = f"{self._url_key_name}/api/v2/observables/extended"
+        headers = {
+            "Accept": "application/json",
+            "User-Agent": "IntelOwl",
+            "Authorization": f"Bearer {access_token}",
+        }
+
         try:
             resp = requests.post(
                 url=url,
